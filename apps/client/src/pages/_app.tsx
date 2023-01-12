dd6269a5e500e1c1a9ed4ec0e91812cb2fd89ade
soumakkdev <soumakkdutta@gmail.com>
2023-01-12 23:37:54 +0530

    React query setup

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -5,16 +5,23 @@ import type { AppProps } from 'next/app'
 import { Inter } from '@next/font/google'
 import { AuthProvider } from '../lib/AuthContext'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const queryClient = new QueryClient()
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<AuthProvider>
-			<div className={inter.className}>
-				<Component {...pageProps} />
-			</div>
-			<Toaster position="bottom-center" />
-		</AuthProvider>
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<div className={inter.className}>
+					<Component {...pageProps} />
+				</div>
+				<Toaster position="bottom-center" />
+			</AuthProvider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	)
 }
